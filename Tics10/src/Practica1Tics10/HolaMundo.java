81009f8d64626ab2b56dbf3625fae0f39a7a3fa9
RAVH <vehur@outlook.com>
2015-10-07 18:22:18 -0500

    Prueba1

---
+++ Tics10/src/Practica1Tics10/HolaMundo.java
@@ -0,0 +1,11 @@
+package Practica1Tics10;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!");
+
+	}
+
+}
